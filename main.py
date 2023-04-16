6d20133ad630f341b9777075051f619302f84a6a
va1afar / jin <va1afar.k@gmail.com>
2023-04-16 23:41:23 +0900

    2023.04.16 commit
    Separated .csv file output function as file.py
    I'm gonna add UI function with using Flask soon.

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from extractors.indeed_scraper import extract_indeed_jobs
-from extractors.wwr_scraper import extract_wwr_jobs
+# from extractors.indeed_scraper import extract_indeed_jobs
+# from extractors.wwr_scraper import extract_wwr_jobs
+# from file import save_to_file
 
-# receive input, merge wwr and indeed datas.
-# and export as a csv file.
-keyword = input("What do you want to search for?")
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-jobs = indeed + wwr
+# # receive input, merge wwr and indeed datas.
+# # and export as a csv file.
+# keyword = input("What do you want to search for?")
+# indeed = extract_indeed_jobs(keyword)
+# wwr = extract_wwr_jobs(keyword)
+# jobs = indeed + wwr
 
-file = open(f"{keyword}.csv", "w")
-file.write("Position,Company,Location,URL\n")
+# save_to_file(keyword, jobs)
 
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
 
+# ---------------------------------------------------
+# gonna make UI for more convinient use
+# with Flask. 
+# ---------------------------------------------------
+
+from flask import Flask
+
+app = Flask("WebScrapper")
+
+@app.route("/") # decorator
+def home():
+    return "hey there!"
+
+app.run("0.0.0.0")
 
 
