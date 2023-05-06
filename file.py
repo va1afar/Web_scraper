037e4acde837688705c5016a89755747ba5ec1de
va1afar / jin <va1afar.k@gmail.com>
2023-05-07 02:26:43 +0900

    2023.05.07 commit
    UI improvement. New function add.

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
-def save_to_file(file_name, jobs):
-    file = open(f"{file_name}.csv", "w")
+def save_to_file(keyword, jobs):
+    file = open(f"{keyword}.csv", "w", encoding='UTF-8')
     file.write("Position,Company,Location,URL\n")
     
     for job in jobs:
--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-# from extractors.indeed_scraper import extract_indeed_jobs
-# from extractors.wwr_scraper import extract_wwr_jobs
-# from file import save_to_file
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.indeed_scraper import extract_indeed_jobs
+from extractors.wwr_scraper import extract_wwr_jobs
+from file import save_to_file
 
-# # receive input, merge wwr and indeed datas.
-# # and export as a csv file.
-# keyword = input("What do you want to search for?")
-# indeed = extract_indeed_jobs(keyword)
-# wwr = extract_wwr_jobs(keyword)
-# jobs = indeed + wwr
+app = Flask(__name__) 
 
-# save_to_file(keyword, jobs)
+db = {} # Results from previous search.
 
 
-# ---------------------------------------------------
-# gonna make UI for more convinient use
-# with Flask. 
-# ---------------------------------------------------
-
-from flask import Flask
-
-app = Flask("WebScrapper")
-
-@app.route("/") # decorator
+@app.route("/") # decorator. Write the function directly below.
 def home():
-    return "hey there!"
+    return render_template("home.html")
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/") 
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return (f"{keyword}.csv file has been downloaded.")
 
 app.run("0.0.0.0")
 
