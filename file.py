5fc06bedc3efbdfe69eb4ac4975414f9b25e62a1
va1afar / jin <va1afar.k@gmail.com>
2023-05-07 02:52:23 +0900

    2023.05.07 modified
    Fix the problem of breaking characters on Excel.

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 def save_to_file(keyword, jobs):
     file = open(f"{keyword}.csv", "w", encoding='UTF-8')
-    file.write("Position,Company,Location,URL\n")
+    file.write(" \ufeffPosition,Company,Location,URL\n")
     
     for job in jobs:
             file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
